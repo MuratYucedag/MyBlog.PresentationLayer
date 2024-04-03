de04d799860a4a5984893c2ab52f9788d4696a6a
MuratYucedag <yucedagmurat23@gmail.com>
2024-04-03 21:18:48 +0300

    ...

---
+++ MyBlog.PresentationLayer/ViewComponents/DefaultViewComponents/_DefaultHeadComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlog.PresentationLayer.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultHeadComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyBlog.PresentationLayer/ViewComponents/DefaultViewComponents/_SidebarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlog.PresentationLayer.ViewComponents.DefaultViewComponents
+{
+    public class _SidebarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
