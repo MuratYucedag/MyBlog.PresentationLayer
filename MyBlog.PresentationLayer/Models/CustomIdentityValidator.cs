46761748f57efa6a429fd9b6164551c892a48e61
MuratYucedag <yucedagmurat23@gmail.com>
2024-04-16 22:19:02 +0300

    ...

---
+++ MyBlog.PresentationLayer/Models/CustomIdentityValidator.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MyBlog.PresentationLayer.Models
+{
+    public class CustomIdentityValidator : IdentityErrorDescriber
+    {
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresLower",
+                Description = "Lütfen en az 1 tane küçük harf girişi yapınız('a'-'z')."
+            };
+        }
+        public override IdentityError PasswordRequiresUpper()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresUpper",
+                Description = "Lütfen en az 1 tane büyük harf girişi yapınız('A'-'Z')."
+            };
+        }
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordTooShort",
+                Description = "Lütfen en az 6 karakter veri girişi yapınız."
+            };
+        }
+
+        public override IdentityError PasswordRequiresNonAlphanumeric()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresNonAlphanumeric",
+                Description = "Lütfen en az 1 tane sembol girişi yapınız."
+            };
+        }
+    }
+}
--- MyBlog.PresentationLayer/Program.cs
+++ MyBlog.PresentationLayer/Program.cs
@@ -4,6 +4,7 @@ using MyBlog.DataAccessLayer.Abstract;
 using MyBlog.DataAccessLayer.Context;
 using MyBlog.DataAccessLayer.EntityFramework;
 using MyBlog.EntityLayer.Concrete;
+using MyBlog.PresentationLayer.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,7 +20,7 @@ builder.Services.AddScoped<ISocialMediaService, SocialMediaManager>();
 builder.Services.AddScoped<ISocialMediaDal, EfSocialMediaDal>();
 
 builder.Services.AddDbContext<BlogContext>();
-builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<BlogContext>();
+builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<BlogContext>().AddErrorDescriber<CustomIdentityValidator>();
 
 builder.Services.AddControllersWithViews();
 
