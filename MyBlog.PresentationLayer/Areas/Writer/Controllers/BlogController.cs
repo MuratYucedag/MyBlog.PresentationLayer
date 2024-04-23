95c38d8d54aff50cf8ea11fe515e0049bae7ba51
MuratYucedag <yucedagmurat23@gmail.com>
2024-04-23 21:44:06 +0300

    ...

--- MyBlog.PresentationLayer/Areas/Writer/Controllers/BlogController.cs
+++ MyBlog.PresentationLayer/Areas/Writer/Controllers/BlogController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using MyBlog.BusinessLayer.Abstract;
 using MyBlog.EntityLayer.Concrete;
 
@@ -10,10 +11,12 @@ namespace MyBlog.PresentationLayer.Areas.Writer.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly IArticleService _articleService;
-        public BlogController(UserManager<AppUser> userManager, IArticleService articleService)
+        private readonly ICategoryService _categoryService;
+        public BlogController(UserManager<AppUser> userManager, IArticleService articleService, ICategoryService categoryService)
         {
             _userManager = userManager;
             _articleService = articleService;
+            _categoryService = categoryService;
         }
         public async Task<IActionResult> MyBlogList()
         {
@@ -45,15 +48,49 @@ namespace MyBlog.PresentationLayer.Areas.Writer.Controllers
         [HttpGet]
         public IActionResult CreateBlog()
         {
+            List<SelectListItem> values = (from x in _categoryService.TGetListAll()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryId.ToString()
+                                           }).ToList();
+            ViewBag.v = values;
             return View();
         }
 
         [HttpPost]
         public async Task<IActionResult> CreateBlog(Article article)
         {
+
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            article.CreatedDate = DateTime.Now;
+            article.AppUserId=user.Id;
+
             _articleService.TInsert(article);
             return RedirectToAction("MyBlogList");
         }
 
     }
 }
+/*
+  List<SelectListItem> values = new List<SelectListItem>();
+
+    // İlk olarak "kategori seçiniz" ifadesini ekleyin
+    values.Add(new SelectListItem
+    {
+        Text = "Kategori Seçiniz",
+        Value = "", // Buraya boş bir değer verin veya istediğiniz bir değeri atayın
+        Selected = true // Varsayılan olarak seçili olsun
+    });
+
+    // Ardından kategori listesini ekleyin
+    values.AddRange(from x in _categoryService.TGetListAll()
+                    select new SelectListItem
+                    {
+                        Text = x.CategoryName,
+                        Value = x.CategoryId.ToString()
+                    });
+
+    ViewBag.v = values;
+ */
